d8b4ed077957494cb4648694a7e2e839cc6f80c0
Younger Bear <shuzhuchengfu@163.com>
2018-10-08 12:53:18 +0800

    创建c++项目

---
+++ MyMusic/app/src/main/java/com/ywl5320/mymusic/MainActivity.java
@@ -0,0 +1,24 @@
+package com.ywl5320.mymusic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+    }
+
+    public native String stringFromJNI();
+}
